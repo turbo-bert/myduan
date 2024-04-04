32b4249365648525df70bd8a9cbe7cfd3b3c0e95
Robert Degen <robert.degen@mailbox.org>
2024-04-04 13:37:51 +0200

    ...

--- src/myduan/__main__.py
+++ src/myduan/__main__.py
@@ -8,14 +8,16 @@ outfile = sys.argv[2]
 
 with open(outfile, 'w') as foutfile:
 
+    # step 0 // binary utf8 safe read
+    lines = []
+    with open(srcfile, 'rb') as f:
+        _blob = f.read()
+    lines = _blob.decode("utf-8", "replace").split("\n")
+
     # step 1 // get tables
-
-
     tbl_names = []
-
-    with open(srcfile, 'r') as f:
-        for line in f:
-            if line.startswith("CREATE TABLE "):
-                tbl_name = line.split("`")[1]
-                tbl_names.append(tbl_name)
-                foutfile.write("tabelle %s\n")
+    for line in lines:
+        if line.startswith("CREATE TABLE "):
+            tbl_name = line.split("`")[1]
+            tbl_names.append(tbl_name)
+            foutfile.write("tabelle %s\n")
