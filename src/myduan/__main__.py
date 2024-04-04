a71531431d09489c64cd1800d4ecb8284659a867
Robert Degen <robert.degen@mailbox.org>
2024-04-04 13:31:49 +0200

    ...

--- src/myduan/__main__.py
+++ src/myduan/__main__.py
@@ -1,3 +1,21 @@
 import sys
 
+#                 1       2
 print("usager: $0 SQLFILE OUTFILE")
+
+srcfile = sys.argv[1]
+outfile = sys.argv[2]
+
+with open(outfile, 'w') as foutfile:
+
+    # step 1 // get tables
+
+
+    tbl_names = []
+
+    with open(srcfile, 'r') as f:
+        for line in f:
+            if line.startswith("CREATE TABLE "):
+                tbl_name = line.split("`")[1]
+                tbl_names.append(tbl_name)
+                foutfile.write("tabelle %s\n")
